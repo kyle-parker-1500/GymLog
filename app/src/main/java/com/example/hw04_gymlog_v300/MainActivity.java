e9aef71d58466fd423e006b768f44a69cc47ebe5
Kyle Parker <kylerparker1500@gmail.com>
2025-10-29 23:52:38 -0700

    finished video 4

--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         String newDisplay = String.format(Locale.US,"Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=-=%n%s", mExercise, mWeight, mReps, currentInfo);
 
         binding.logDisplayTextView.setText(newDisplay);
+        Log.i(TAG, repository.getAllLogs().toString());
     }
 
     private void getInformationFromDisplay() {
