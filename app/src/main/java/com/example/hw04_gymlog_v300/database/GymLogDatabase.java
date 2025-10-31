f706ea0fddfc67478e877f04c853cc38441fa53b
Kyle Parker <kylerparker1500@gmail.com>
2025-10-31 00:27:36 -0700

    didn't fail like I thought I would

--- app/src/main/java/com/example/hw04_gymlog_v300/database/GymLogDatabase.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/database/GymLogDatabase.java
@@ -21,11 +21,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @TypeConverters(LocalDateTypeConverter.class)
-@Database(entities = {GymLog.class, User.class}, version = 2, exportSchema = false)
+@Database(entities = {GymLog.class, User.class}, version = 1, exportSchema = false)
 public abstract class GymLogDatabase extends RoomDatabase {
 
-    public static final String USER_TABLE = "user_table";
-    private static final String DATABASE_NAME = "GymLog_database";
+    public static final String USER_TABLE = "userTable";
+    private static final String DATABASE_NAME = "GymLogDatabase";
     // names of tables go here
     public static final String GYM_LOG_TABLE = "gymLogTable";
 
