a04194052fd6ab983e53e5ffd1095ae9463b472d
Kyle Parker <kylerparker1500@gmail.com>
2025-11-01 02:04:38 -0700

    finished vid10

--- app/src/main/java/com/example/hw04_gymlog_v300/LoginActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/LoginActivity.java
@@ -4,13 +4,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.hw04_gymlog_v300.database.GymLogRepository;
+import com.example.hw04_gymlog_v300.database.entities.User;
 import com.example.hw04_gymlog_v300.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
+
+    private GymLogRepository repository;
+
+    private User user = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +25,47 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        repository = GymLogRepository.getRepository(getApplication());
+
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), 0);
-                startActivity(intent);
+                if (!verifyUser()) {
+                    toastMaker("Invalid credentials.");
+                } else {
+                    Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), user.getId());
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    private boolean verifyUser() {
+        String username = binding.userNameLoginEditText.getText().toString();
+        if (username.isEmpty()) {
+            toastMaker("Username may not be blank.");
+            return false;
+        }
+        user = repository.getUserByUsername(username);
+        if (user != null) {
+            String password = binding.passwordLoginEditText.getText().toString();
+            if (password.equals(user.getPassword())) {
+                return true;
+            } else {
+                toastMaker("Invalid password.");
+                return false;
+            }
+        }
+        toastMaker(String.format("No %s found.", username));
+        return false;
+    }
+
+    /**
+     * prints Toast to android <b>for</b> passed message
+     * @param message
+     */
+    private void toastMaker(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     static Intent loginIntentFactory(Context context) {
--- app/src/main/java/com/example/hw04_gymlog_v300/database/UserDAO.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/database/UserDAO.java
@@ -26,4 +26,7 @@ public interface UserDAO {
 
     @Query("DELETE from " + GymLogDatabase.USER_TABLE)
     void deleteAll();
+
+    @Query("SELECT * from " + GymLogDatabase.USER_TABLE + " WHERE username == :username")
+    User getUserByUsername(String username);
 }
